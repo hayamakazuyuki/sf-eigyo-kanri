54852ec686690264c45e05639787a02f3f9466ed
hayamakazuyuki <hayama@sfinter.com>
2021-07-14 02:09:30 +0900

    database  connected

--- record/app.py
+++ record/app.py
@@ -1,6 +1,10 @@
 import os
 from flask import Flask, request, abort
 
+from .models import Post
+from .extentions import db
+
+
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
@@ -13,6 +17,8 @@ def create_app(config_file='settings.py'):
     app = Flask(__name__)
 
     app.config.from_pyfile(config_file)
+
+    db.init_app(app)
 
     line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
     handler = WebhookHandler(CHANNEL_SECRET)
@@ -37,9 +43,17 @@ def create_app(config_file='settings.py'):
 
     @handler.add(MessageEvent, message=TextMessage)
     def handle_message(event):
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text='完了'))
-            # TextSendMessage(text=event.message.text))
+        user_id = event.source.user_id
+        msg_body = event.message.text
+
+        post = Post(
+            line_user_id=user_id,
+            message=msg_body
+        )
+        db.session.add(post)
+        db.session.commit()
+
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=user_id))
+        # TextSendMessage(text=event.message.text))
 
     return app
--- record/models.py
+++ record/models.py
@@ -1,9 +1,9 @@
+from datetime import datetime
 from .extentions import db
 
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    line_message_id = db.Column(db.String(255), nullable=False)
     line_user_id = db.Column(db.String(255), nullable=False)
     message = db.Column(db.String(255), nullable=False)
-    created_at = db.Column(db.Datetime, nullable=False)
+    # posted_at = db.Column(db.Datetime, nullable=False, default=datetime.now)
