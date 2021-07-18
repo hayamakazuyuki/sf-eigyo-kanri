4aac2680d822f7f1728ed97f1416800298b8dabc
hayamakazuyuki <hayama@sfinter.com>
2021-07-18 17:19:46 +0900

    timezone set to JST

--- record/models.py
+++ record/models.py
@@ -1,5 +1,7 @@
-from datetime import datetime
+from datetime import datetime, timedelta, timezone
 from .extentions import db
+
+JST = timezone(timedelta(hours=+9), 'JST')
 
 
 class Post(db.Model):
@@ -9,5 +11,5 @@ class Post(db.Model):
     group_id = db.Column(db.String(255), nullable=True)
     message_id = db.Column(db.String(255), nullable=False)
     message = db.Column(db.Text(), nullable=False)
-    posted_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    posted_at = db.Column(db.DateTime, nullable=False, default=datetime.now(JST))
     checked = db.Column(db.Integer, nullable=True)
